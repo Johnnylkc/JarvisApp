9cb7fcd31a990ba7c4f8f6a17aa1b07858314a07
Johnnylkc <momozzz1231@gmail.com>
2016-02-24 15:49:28 +0800

    02/24 15:50

---
+++ JarvisApp/MainCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainCell.swift
+//  JarvisApp
+//
+//  Created by 劉坤昶 on 2016/2/24.
+//  Copyright © 2016年 JohnnyKetchup. All rights reserved.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
