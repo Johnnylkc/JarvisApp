7951961dca741a1deed39d81e7ca49e49f5d0c0c
Johnnylkc <momozzz1231@gmail.com>
2016-02-29 17:02:57 +0800

    229 17:00

--- JarvisApp/ThirdCell.swift
+++ JarvisApp/ThirdCell.swift
@@ -13,7 +13,7 @@ class ThirdCell: UITableViewCell {
     
     let basicView = UIView()
     let bigImage = UIImageView()
-    let titleLabel = UILabel()
+    let titleTextView = UITextView()
     
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?)
@@ -27,8 +27,12 @@ class ThirdCell: UITableViewCell {
         bigImage.backgroundColor = UIColor.lightGrayColor()
         basicView.addSubview(bigImage)
         
-        titleLabel.backgroundColor = UIColor.redColor()
-        basicView.addSubview(titleLabel)
+        
+        titleTextView.backgroundColor = UIColor.redColor()
+        titleTextView.scrollEnabled = false
+        titleTextView.editable = false
+        titleTextView.font = UIFont.systemFontOfSize(18)
+        basicView.addSubview(titleTextView)
         
         
         autoLayout()
@@ -37,32 +41,34 @@ class ThirdCell: UITableViewCell {
     
     func autoLayout()
     {
+        
         basicView.translatesAutoresizingMaskIntoConstraints = (false)
         bigImage.translatesAutoresizingMaskIntoConstraints = (false)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = (false)
+        titleTextView.translatesAutoresizingMaskIntoConstraints = (false)
         
         
-        let dict = ["basicView":basicView,"bigImage":bigImage,"titleLabel":titleLabel]
+        let dic = ["basicView":basicView,"bigImage":bigImage,"titleTextView":titleTextView]
         
         
         ////basicView
-        let basicViewH = NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[basicView]-10-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dict)
+        let basicViewH = NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[basicView]-10-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
         contentView.addConstraints(basicViewH)
         
-        let basicViewV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-10-[basicView]-10-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dict)
+        let basicViewV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-10-[basicView]-10-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
         contentView.addConstraints(basicViewV)
         
-        ////bigImage & titleLael 前著在左 後者在右 兩者的寬是相同的
-        let bigImageH = NSLayoutConstraint.constraintsWithVisualFormat("H:|[bigImage(==titleLabel)][titleLabel(==bigImage)]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dict)
+        
+        ////bigImage & titleTextView 水平位置和寬度
+        let bigImageH = NSLayoutConstraint.constraintsWithVisualFormat("H:|[bigImage(==titleTextView)][titleTextView(==bigImage)]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
         basicView.addConstraints(bigImageH)
         
-        let bigImageV = NSLayoutConstraint.constraintsWithVisualFormat("V:|[bigImage]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dict)
+        ////bigImage 垂直位置和高度
+        let bigImageV = NSLayoutConstraint.constraintsWithVisualFormat("V:|[bigImage]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
         basicView.addConstraints(bigImageV)
         
-        let titleLabelV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-10-[titleLabel(30)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dict)
-        basicView.addConstraints(titleLabelV)
-        
-        
+        ////titleTextView 垂直位置和高度
+        let titleTextViewV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-10-[titleTextView(30)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: dic)
+        basicView.addConstraints(titleTextViewV)
         
         
     }
